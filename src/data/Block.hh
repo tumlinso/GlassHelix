9a47d253bc9b3d9070d541cc91be8ffdca6a34c4
tumlinson <gavin.tumlinson@campus.lmu.de>
2025-05-15 15:07:11 +0200

    var block

---
+++ src/data/Block.hh
@@ -0,0 +1,13 @@
+#ifndef GLASSHELIX_BLOCK_HH
+#define GLASSHELIX_BLOCK_HH
+
+static inline const unsigned long l3cacheMB = 16;
+static inline constexpr unsigned long chunkSize = l3cacheMB * 1024 * 1024;
+
+template<typename T>
+class Block {
+
+};
+
+
+#endif //GLASSHELIX_BLOCK_HH
--- src/data/Dataset.cc
+++ src/data/Dataset.cc
@@ -1,23 +1,5 @@
 #include "Dataset.hh"
 
-void Dataset::readTokenDictionary(std::string filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) throw std::runtime_error("Error opening Token Dictionary File: " + filename);
-
-    std::string line;
-    std::string token, index_string;
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-
-        if (!std::getline(iss, token, ',')) continue;
-        if (!std::getline(iss, index_string, ',')) continue;
-
-        unsigned long index = std::stoi(index_string);
-        this->Tokens.emplace(token, index);
-    }
-    file.close();
-}
-
 void Dataset::readData(std::string filename) {
 
 }
