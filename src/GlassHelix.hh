fe2253ba9f85d178932ca595c03d3f503cd92040
tumlinson <gavin.tumlinson@campus.lmu.de>
2025-05-15 10:18:30 +0200

    Dset token load

--- src/GlassHelix.hh
+++ src/GlassHelix.hh
@@ -1,9 +1,17 @@
 #ifndef GLASSHELIX_GLASSHELIX_HH
 #define GLASSHELIX_GLASSHELIX_HH
 
+#include <cstddef>
+#include <iostream>
+
+#include "data/Dataset.hh"
+
 
 class GlassHelix {
 
+    explicit GlassHelix();
+
+    void train(Dataset &TrainingData)
 };
 
 
--- src/data/Dataset.hh
+++ src/data/Dataset.hh
@@ -1,13 +1,20 @@
 #ifndef GLASSHELIX_DATASET_HH
 #define GLASSHELIX_DATASET_HH
 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+
 #include <arrow/api.h>
 #include <arrow/io/api.h>
 #include <arrow/ipc/api.h>
-#include <iostream>
-#include <string>
 
 #include "Dictionary.hh"
+
+static inline const unsigned long l3cacheMB = 16;
+static inline constexpr unsigned long chunkSize = l3cacheMB * 1024 * 1024;
 
 class Dataset {
 private:
@@ -16,7 +23,6 @@ public:
     Dictionary<std::string> Tokens;
 
     Dataset(std::string PathTokenDict);
-
 };
 
 
