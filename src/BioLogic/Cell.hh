747a6c3f2cf3b35b6d7afac36a191bd2b4a86a78
tumlinson <gavin.tumlinson@campus.lmu.de>
2025-05-15 14:14:42 +0200

    Cell class ptr to dset

--- src/BioLogic/Cell.hh
+++ src/BioLogic/Cell.hh
@@ -1,9 +1,17 @@
 #ifndef GLASSHELIX_CELL_HH
 #define GLASSHELIX_CELL_HH
 
+#include <iostream>
+#include <vector>
+#include <string>
 
+static const unsigned long length = 1000;
+
+template<typename TokenType>
 class Cell {
-
+public:
+    Cell() = default;
+    TokenType *tokens;
 };
 
 #include "Cell.inl"
