38ea40f5ad8f895399c9a86070e0fa657ed912c9
tumlinson <gavin.tumlinson@campus.lmu.de>
2025-05-27 13:50:00 +0200

    fix cuda

--- src/models/GlassEtch/include/types.cuh
+++ src/models/GlassEtch/include/types.cuh
@@ -8,5 +8,13 @@ using fp16  = __half;
 using ui16  = uint16_t;
 using ui32  = uint32_t;
 
+template<typename T>
+struct DeviceMatrix {
+    T*   data;
+    int  ld;
+    int  rows, cols;
+    __device__ T& operator()(int r,int c) { return data[r*ld + c]; }
+    __device__ const T& operator()(int r,int c) const { return data[r*ld + c]; }
+};
 
 #endif //GLASSHELIX_GLASSETCH_TYPES_CUH
