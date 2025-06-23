c8c6aaafea9cea40c9d4cce4b996d30e4ce1c183
tumlinson <gavin.tumlinson@campus.lmu.de>
2025-06-23 16:05:02 +0200

    matrix agnostic

--- src/include/sigmoid.cuh
+++ src/include/sigmoid.cuh
@@ -4,10 +4,5 @@
 #include "macros.cuh"
 
 radical double sigmoid(double x) { return 1.0 / (1.0 + exp(-x)); }
-radical void sigmoid(double *in, double *out, unsigned int len) {
-    for (unsigned int i = 0; i < len; ++i) {
-        out[i] = sigmoid(in[i]);
-    }
-}
 
 #endif //GLASSHELIX_SIGMOID_CUH
